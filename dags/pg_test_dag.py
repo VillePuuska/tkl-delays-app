f73c8f58ab144d9edf3d6e6a0006aea43532a9fd
Ville Puuska <puuskaville@gmail.com>
2023-11-28 17:27:33 +0200

    setup and add dag to test local postgres db connection

---
+++ dags/pg_test_dag.py
@@ -0,0 +1,30 @@
+import datetime
+
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+
+def select_to_df():
+    hook = PostgresHook()
+    df = hook.get_pandas_df(sql="SELECT * FROM testing_table;")
+    print(df)
+    return df.to_string()
+
+with DAG(
+    dag_id="postgres_test",
+    description="Testing connection to local postgres",
+    start_date=datetime.datetime(2023, 11, 28),
+    schedule=None,
+) as dag:
+    insert_task = PostgresOperator(
+        task_id="insert_task",
+        sql="INSERT INTO testing_table (col1) VALUES ('{{ ts }}');",
+    )
+
+    select_df_task = PythonOperator(
+        task_id='select_star_to_df',
+        python_callable=select_to_df,
+    )
+
+    insert_task >> select_df_task
