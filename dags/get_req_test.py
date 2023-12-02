539aa0bec9ad31676ac66946886f882675553d44
Ville Puuska <puuskaville@gmail.com>
2023-12-02 17:17:12 +0200

    setup and test Http connection to JourneysAPI with hook

---
+++ dags/get_req_test.py
@@ -0,0 +1,25 @@
+import datetime
+import os
+
+from airflow import DAG
+from airflow.hooks.filesystem import FSHook
+from airflow.providers.http.hooks.http import HttpHook
+from airflow.operators.python import PythonOperator
+
+def get_and_save(**kwargs):
+    response = HttpHook(method='GET').run()
+    filepath = FSHook().get_path()
+    with open(os.path.join(filepath, f'{str(kwargs["ts_nodash"])}-api-call.txt'), 'w') as f:
+        f.write(response.text)
+
+with DAG(
+    dag_id="get_and_save_test",
+    description="Testing HTTP GET from JourneysAPI",
+    start_date=datetime.datetime(2023, 12, 2),
+    schedule=None,
+) as dag:
+    save_file_task = PythonOperator(
+        task_id="get_and_save_task",
+        python_callable=get_and_save,
+        provide_context=True,
+    )
