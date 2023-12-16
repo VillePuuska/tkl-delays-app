69b03f0b1718fe911a52ce887af9e9909370d5e7
Ville Puuska <puuskaville@gmail.com>
2023-12-16 14:38:57 +0200

    add latest table sql, etl clear and insert new data to latest

--- dags/ETL_dag.py
+++ dags/ETL_dag.py
@@ -18,10 +18,24 @@ def get_and_save(**kwargs):
     with open(os.path.join(filepath, f'{str(kwargs["ts_nodash"])}-api-call.txt'), 'w') as f:
         f.write(response.text)
 
+def delay_sec(s: str) -> int:
+    if s[0] == '-':
+        neg = -1
+        s = s[11:len(s)-5].split('M')
+    else:
+        neg = 1
+        s = s[10:len(s)-5].split('M')
+    return neg * (60*int(s[0]) + int(s[1]))
+
+def stop_id(s: str) -> str:
+    return s[-4:]
+
 def body_to_df(body: list, logging: int = 0) -> pd.DataFrame:
     """
     Function that takes the body of the GET request to JourneysAPI Vehicle Activity endpoint
     and flattens it to a dataframe.
+
+    One row per stop in onward calls.
 
     params:
         - body: requests.get(URL).json()['body']
@@ -29,17 +43,6 @@ def body_to_df(body: list, logging: int = 0) -> pd.DataFrame:
                     >= 1 - print number of errors when flattening,
                     >= 2 print every error message
     """
-    def delay_sec(s: str) -> int:
-        if s[0] == '-':
-            neg = -1
-            s = s[11:len(s)-5].split('M')
-        else:
-            neg = 1
-            s = s[10:len(s)-5].split('M')
-        return neg * (60*int(s[0]) + int(s[1]))
-
-    def stop_id(s: str) -> str:
-        return s[-4:]
 
     header = ['Recorded_At', 'Line', 'Direction', 'Date', 'Lon',
               'Lat', 'Delay', 'Departure_Time', 'Stop', 'Stop_Order']
@@ -97,7 +100,55 @@ def flatten_and_upsert_function(**kwargs):
                                      'Departure_Time',
                                      'Stop'],
                      )
+
+def body_to_df_latest(body: list, logging: int = 0) -> pd.DataFrame:
+    """
+    Function that takes the body of the GET request to JourneysAPI Vehicle Activity endpoint
+    and flattens it to a dataframe.
+
+    Only one row per bus/tram in body.
+
+    params:
+        - body: requests.get(URL).json()['body']
+        - logging:  <= 0 - no logging/printing,
+                    >= 1 - print number of errors when flattening,
+                    >= 2 print every error message
+    """
+
+    header = ['Recorded_At', 'Line', 'Direction', 'Date', 'Lon',
+              'Lat', 'Delay', 'Departure_Time']
     
+    lines = []
+    broken_lines = 0
+    for bus in body:
+        try:
+            rec_ts = bus['recordedAtTime']
+            line = bus['monitoredVehicleJourney']['lineRef']
+            direction = bus['monitoredVehicleJourney']['directionRef']
+            date = bus['monitoredVehicleJourney']['framedVehicleJourneyRef']['dateFrameRef']
+            lon = bus['monitoredVehicleJourney']['vehicleLocation']['longitude']
+            lat = bus['monitoredVehicleJourney']['vehicleLocation']['latitude']
+            delay = delay_sec(bus['monitoredVehicleJourney']['delay'])
+            dep_time = bus['monitoredVehicleJourney']['originAimedDepartureTime']
+
+            lines.append([rec_ts, line, direction, date, lon, lat, delay, dep_time])
+        except Exception as e:
+            if logging > 1:
+                print(e)
+            broken_lines += 1
+    if logging > 0:
+        print('Total # of broken records (entire vehicle journeys):', broken_lines)
+    
+    return pd.DataFrame(lines, columns=header)
+
+def flatten_and_insert_latest_function(**kwargs):
+    filepath = FSHook().get_path()
+    with open(os.path.join(filepath, f'{str(kwargs["ts_nodash"])}-api-call.txt'), 'r') as f:
+        contents = json.loads(f.read())
+    body = contents['body']
+    df = body_to_df_latest(body, 2)
+    hook = PostgresHook()
+    hook.insert_rows('latest', list(df.itertuples(index=False)))
 
 with DAG(
     dag_id="etl",
@@ -118,6 +169,17 @@ with DAG(
         provide_context=True
     )
 
+    delete_latest = PostgresOperator(
+        task_id='delete_latest_task',
+        sql='DELETE FROM latest;'
+    )
+
+    flatten_and_insert_latest = PythonOperator(
+        task_id='flatten_and_insert_latest_data_task',
+        python_callable=flatten_and_insert_latest_function,
+        provide_context=True
+    )
+
     archive_json = BashOperator(
         task_id='archive_saved_json',
         bash_command='mv $filepath/$filename $filepath/archive/$filename',
@@ -125,4 +187,6 @@ with DAG(
              'filename':'{{ ts_nodash }}-api-call.txt'}
     )
 
-    get_data_task >> flatten_and_upsert >> archive_json
+    get_data_task >> [flatten_and_upsert, delete_latest]
+    delete_latest >> flatten_and_insert_latest
+    [flatten_and_upsert, flatten_and_insert_latest] >> archive_json
