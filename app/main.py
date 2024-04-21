d76f58777e04844d34fa1c562efaa2362634d6f0
Ville Puuska <40150442+VillePuuska@users.noreply.github.com>
2024-04-21 12:44:52 +0000

    add map to streamlit app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from utils.db_operations import get_buses_and_stops_aggregated
 
 df_bus_agg, df_stop_agg = get_buses_and_stops_aggregated()
+
+st.header("Latest bus locations.")
+
+try:
+    with open("/workspaces/tkl-delays-app/data/map.html") as f:
+        map_html = f.read()
+    components.html(html=map_html, height=750, width=1000)
+except FileNotFoundError:
+    st.warning(
+        "Could not find map.html. Make sure the filepath in `app/main.py` is correct."
+    )
 
 st.write(df_bus_agg)
 st.write(df_stop_agg)
